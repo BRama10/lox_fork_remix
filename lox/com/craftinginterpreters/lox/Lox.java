5cf167c1b2d54ecf9f5acd81b1572a6a1bb56672
BRama10 <81198936+BRama10@users.noreply.github.com>
2022-02-14 11:57:02 -0500

    Update Lox.java

--- lox/com/craftinginterpreters/lox/Lox.java
+++ lox/com/craftinginterpreters/lox/Lox.java
@@ -13,10 +13,17 @@ public class Lox {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
       System.exit(64); 
-    } else if (args.length == 1) {
+    } 
+    else if (args.length == 1) {
       runFile(args[0]);
-    } else {
+    } 
+    else {
       runPrompt();
     }
+  }
+  
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
   }
 }
