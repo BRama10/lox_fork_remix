fa6e1b002c002eb4ff94490fb8eb3486edbc9a18
BRama10 <81198936+BRama10@users.noreply.github.com>
2022-02-14 12:07:52 -0500

    Update Lox.java
    Primitive error handling.

--- lox/com/craftinginterpreters/lox/Lox.java
+++ lox/com/craftinginterpreters/lox/Lox.java
@@ -48,5 +48,16 @@ public class Lox {
       System.out.println(token);
     }
   }
+  
+   static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where,
+                             String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
 }
 
