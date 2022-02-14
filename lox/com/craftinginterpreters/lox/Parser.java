79d2f04dfea551e0a508da2c485e4b164ed035d2
BRama10 <81198936+BRama10@users.noreply.github.com>
2022-02-14 18:44:46 -0500

    Update Parser.java
    Completed parser comparison methods for 4 order of precedence's.

--- lox/com/craftinginterpreters/lox/Parser.java
+++ lox/com/craftinginterpreters/lox/Parser.java
@@ -40,6 +40,56 @@ class Parser {
     return expr;
   }
   
+  private Expr term() {
+    Expr expr = factor();
+
+    while (match(MINUS, PLUS)) {
+      Token operator = previous();
+      Expr right = factor();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+  
+  private Expr factor() {
+    Expr expr = unary();
+
+    while (match(SLASH, STAR)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+  
+  private Expr unary() {
+    if (match(BANG, MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
+
+    return primary();
+  }
+  
+  private Expr primary() {
+    if (match(FALSE)) return new Expr.Literal(false);
+    if (match(TRUE)) return new Expr.Literal(true);
+    if (match(NIL)) return new Expr.Literal(null);
+
+    if (match(NUMBER, STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+
+    if (match(LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(RIGHT_PAREN, "Expect ')' after expression.");
+      return new Expr.Grouping(expr);
+    }
+  }
+  
   private boolean match(TokenType... types) {
     for (TokenType type : types) {
       if (check(type)) {
