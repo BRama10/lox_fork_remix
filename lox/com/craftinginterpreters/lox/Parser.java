2bca62d2543afedd32e0216e9a5f38fe35be5af5
BRama10 <81198936+BRama10@users.noreply.github.com>
2022-02-14 18:51:00 -0500

    Update Parser.java
    resolving synchronization and throwing errors during writing up of parser
    
    added method that inits parser

--- lox/com/craftinginterpreters/lox/Parser.java
+++ lox/com/craftinginterpreters/lox/Parser.java
@@ -16,6 +16,14 @@ class Parser {
     this.tokens = tokens;
   }
   
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
+  
   private Expr expression() {
     return equality();
   }
@@ -92,6 +100,8 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+    
+    throw error(peek(), "Expect expression.");
   }
   
   private boolean match(TokenType... types) {
@@ -146,4 +156,25 @@ class Parser {
     }
   }
   
+  private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+      advance();
+    }
+  }
 }
