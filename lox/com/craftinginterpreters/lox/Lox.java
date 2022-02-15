3bfa5bdea1837eda1205828a8bbb5eefbb8c8c02
BRama10 <81198936+BRama10@users.noreply.github.com>
2022-02-15 11:29:37 -0500

    Update Lox.java
    Connect Interpreter to the main Lox class.

--- lox/com/craftinginterpreters/lox/Lox.java
+++ lox/com/craftinginterpreters/lox/Lox.java
@@ -9,8 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
-  
+  static boolean hadRuntimeError = false;
   
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -29,6 +30,7 @@ public class Lox {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
   
   private static void runPrompt() throws IOException {
@@ -53,14 +55,21 @@ public class Lox {
 
     // Stop if there was a syntax error.
     if (hadError) return;
-
-    System.out.println(new AstPrinter().print(expression));
+    
+    interpreter.interpret(expression);
+    //System.out.println(new AstPrinter().print(expression));
   }
   
   static void error(int line, String message) {
     report(line, "", message);
   }
-
+  
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
+  }
+  
   private static void report(int line, String where,
                              String message) {
     System.err.println(
